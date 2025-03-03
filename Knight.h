3d85262ee65e2569afac2998f7df3b7c188539df
Wiktor Wdowczyk <wiktorwdow@gmail.com>
2025-03-03 12:15:37 +0100

    Add files via upload

--- Knight.h
+++ Knight.h
@@ -7,7 +7,7 @@ class Knight : public Piece{
     public:
         Knight(int id, bool isWhite, Position currentPosition, Board& board);
 
-        void calculateAvailableMoves() override;
+        void findMoveInDirection(std::pair<int, int> direction) override;
         void scanForPin(Position startPosition, int dx, int dy) override;
 };
 
