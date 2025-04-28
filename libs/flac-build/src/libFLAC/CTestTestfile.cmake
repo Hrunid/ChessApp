c9102d5f45db38d93e2236babb470d4c9167ebc6
Hrunid <wiktorwdow@gmail.com>
2025-04-28 16:44:47 +0200

    Linked TGUI & SFML

--- libs/flac-build/src/libFLAC/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/wiktw/OneDrive/Dokumenty/c++/SzachyZId/ChessApp/libs/flac-src/src/libFLAC
-# Build directory: C:/Users/wiktw/OneDrive/Dokumenty/c++/SzachyZId/ChessApp/libs/flac-build/src/libFLAC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
